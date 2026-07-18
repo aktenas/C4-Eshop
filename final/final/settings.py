e5f37c9f6f4aafdfc23432483526d2108d98d077
aktenas <a.ktenas@acg.edu>
2026-07-18 12:57:23 +0300

    configuration for deployment

--- final/final/settings.py
+++ final/final/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bydltv-1bh6o9nwzl1+t&9=70*#0p^18uj7my#1g4kbp@m%1iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['c4tattoo.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',

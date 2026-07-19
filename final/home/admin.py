a9b4c3d8db5890541b23babcb79cea0f31227984
aktenas <a.ktenas@acg.edu>
2026-07-19 16:03:59 +0300

    home reviews admin adition

--- final/home/admin.py
+++ final/home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Booking, Service
+from .models import Booking, Service, Review
 
 
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Booking)
+admin.site.register(Review)
